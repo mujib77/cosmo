677b59074aa25f67c647123ca4df7c61c5b60052
mujib77 <mujibkhanpathan6@gmail.com>
2026-05-16 10:23:54 +0530

    add bubbletea model with tick and data fetching

---
+++ internal/ui/model.go
@@ -0,0 +1,125 @@
+package ui
+
+import (
+	"context"
+	"time"
+
+	"github.com/charmbracelet/bubbletea"
+	"github.com/mujib77/cosmo/internal/db"
+)
+
+type Model struct {
+	db *db.DB
+	overview *db.OverviewStats
+	queries []db.ActiveQuery
+	walStats *db.WALStats
+	locks []db.LockInfo
+	width int
+	height int
+	err error
+	loading bool
+	activePanel int
+}
+
+type tickMsg time.Time
+type dataMsg struct {
+	overview *db.OverviewStats
+	queries []db.ActiveQuery
+	walStats *db.WALStats
+	locks []db.LockInfo
+	err error
+}
+
+func New(database *db.DB) Model {
+	return Model{db: database, loading: true,}
+}
+
+func (m Model) Init() tea.Cmd{
+	return tea.Batch(
+		m.fetchData(),
+		tick(),
+	)
+}
+
+func tick() tea.Cmd {
+	return tea.Tick(2*time.Second, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
+}
+
+func (m Model) fetchData() tea.Cmd {
+	return func() tea.Msg {
+		ctx := context.Background()
+
+		overview, err := m.db.GetOverviewStats(ctx)
+		if err != nil {
+			return dataMsg{err: err}
+		}
+
+		queries, err := m.db.GetActiveQueries(ctx)
+		if err != nil {
+			return dataMsg{err: err}
+		}
+
+		walStats, err := m.db.GetWALStats(ctx)
+		if err != nil {
+			return dataMsg{err: err}
+		}
+
+		locks, err := m.db.GetLocks(ctx)
+		if err != nil {
+			return dataMsg{err: err}
+		}
+
+		return dataMsg{
+			overview: overview,
+			queries: queries,
+			walStats: walStats,
+			locks: locks,
+		}
+	}
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+			case "q", "ctrl+c":
+				return m, tea.Quit
+			case "tab":
+				m.activePanel = (m.activePanel + 1) % 4
+			}
+		
+		case tea.WindowSizeMsg:
+			m.width = msg.Width
+			m.height = msg.Height
+
+	    case tickMsg:
+			return m, tea.Batch(m.fetchData(), tick())
+
+		case dataMsg:
+			m.loading = false
+			if msg.err != nil {
+				m.err = msg.err
+				return m, nil
+			}
+			m.overview = msg.overview
+			m.queries = msg.queries
+			m.walStats = msg.walStats
+			m.locks = msg.locks
+		}
+	return m, nil
+	}
+
+func (m Model) View() string {
+	if m.loading {
+		return "\n connecting to postgres...\n"
+	}
+	if m.err != nil {
+		return "\n error: " + m.err.Error() + "\n"
+	}
+	if m.width == 0 {
+		return ""
+	}
+	return RenderDashboard(m)
+}
